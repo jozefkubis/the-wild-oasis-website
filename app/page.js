1a1b2df339d3b8ab9a85e9b8f1d321149e631cf7
jozef kubis <kubis.jozef@outlook.com>
2024-12-16 18:41:44 +0100

    Navigating Between Pages

--- app/page.js
+++ app/page.js
@@ -1,6 +1,14 @@
+import Link from "next/link"
+import Navigation from "./components/Navigation"
 
 export default function Page() {
   return (
-    <h1>The Wild Oasis. Welcome to paradise.</h1>
-  );
+    <div>
+      <Navigation />
+
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+
+      <Link href="/cabins">Explore luxury Cabins</Link>
+    </div>
+  )
 }
