0df1702eeb83d57c7e20e28d2cc9ac7c517c6ebe
jozef kubis <kubis.jozef@outlook.com>
2024-12-24 13:42:02 +0100

    Using the Context API for State Management

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,8 +1,9 @@
 "use client"
 
-import { isWithinInterval } from "date-fns"
+import { isWithinInterval, set } from "date-fns"
 import { DayPicker } from "react-day-picker"
 import "react-day-picker/dist/style.css"
+import { useReservation } from "./ReservationContext"
 
 function isAlreadyBooked(range, datesArr) {
   return (
@@ -15,12 +16,13 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ settings, bookedDates, cabin }) {
+  const { range, setRange, resetRange } = useReservation()
+
   // CHANGE
   const regularPrice = 23
   const discount = 23
   const numNights = 23
   const cabinPrice = 23
-  const range = { from: null, to: null }
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings
@@ -30,11 +32,13 @@ function DateSelector({ settings, bookedDates, cabin }) {
       <DayPicker
         className="pt-12 place-self-center"
         mode="range"
+        // onSelect={(range) => setRange(range)}
+        onSelect={setRange}
+        selected={range}
         min={minBookingLength + 1}
         max={maxBookingLength}
-        fromMonth={new Date()}
-        fromDate={new Date()}
-        toYear={new Date().getFullYear() + 5}
+        defaultMonth={new Date()}
+        maxDate={new Date(new Date().getFullYear() + 5, 11, 31)}
         captionLayout="dropdown"
         numberOfMonths={2}
       />
