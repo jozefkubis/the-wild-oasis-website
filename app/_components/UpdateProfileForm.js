cac6f5545ea378a96ac4ba8cc68d90a819edaf5d
jozef kubis <kubis.jozef@outlook.com>
2025-01-17 18:27:24 +0100

    Displaying a Loading Indicator: The useFromStatus Hook

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react"
 import { updateGuest } from "../_lib/actions"
+import { useFormStatus } from "react-dom"
 
 function UpdateProfileForm({ guest, children }) {
   const [count, setCount] = useState()
@@ -9,7 +10,10 @@ function UpdateProfileForm({ guest, children }) {
   const { fullName, email, nationality, nationalID, countryFlag } = guest
 
   return (
-    <form action={updateGuest} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
@@ -52,11 +56,22 @@ function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  )
+}
+
+function Button() {
+  const { pending } = useFormStatus()
+
+  return (
+    <button
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
   )
 }
 
