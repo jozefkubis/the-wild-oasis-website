e67b558c4731956efe75755dd7a0ab77a3e692d0
JozefK <164421631+jozefkubis@users.noreply.github.com>
2025-01-17 15:01:48 +0100

    Updating the Profile Using a Server Action

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,26 @@
 "use server"
 
-import { signIn, signOut } from "./auth"
+import { auth, signIn, signOut } from "./auth"
+import { supabase } from "./supabase"
+
+export async function updateGuest(formData) {
+    const session = await auth()
+    if (!session) throw new Error("You must be logged in to update your guest profile.")
+
+    const nationalID = formData.get("nationalID")
+    const [nationality, countryFlag] = formData.get("nationality").split('%')
+
+    if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) throw new Error("Please provide valid national ID.")
+
+    const updateData = { nationalID, nationality, countryFlag }
+
+    const { data, error } = await supabase
+        .from("guests")
+        .update(updateData)
+        .eq("id", session.user.guestId)
+
+    if (error) throw new Error("Guest could not be updated")
+}
 
 export async function signInAction() {
     await signIn("google", { redirectTo: "/account" })
